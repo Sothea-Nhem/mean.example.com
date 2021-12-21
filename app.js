f6b01e126a8eaeba12757f5cd0adbddb260e7478
Sothea Nhem <goddess.of.light.808@gmail.com>
2021-12-21 14:24:48 -0600

    Implement a route for api/users

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var config = require('./config.dev');
+var apiUsersRouter = require('./routes/api/users');
 
-//Test the file
-console.log(config);
+var mongoose = require('mongoose');
+mongoose.connect(config.mongodb, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
 var app = express();
 
 // view engine setup
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/users', apiUsersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
